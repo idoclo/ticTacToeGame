ef8d105b72b8445a9d37cfbd47d78bbb9a607f17
Preda Anuvatnujotikul <preda.anu@gmail.com>
2017-12-23 14:59:54 -0800

    Drew out Scoreboard component on front end. Now need to use HTTP request to fetch top three scoring players from the back end.

--- client/src/Components/Scoreboard.jsx
+++ client/src/Components/Scoreboard.jsx
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react';
 
 
 class Scoreboard extends Component {
+  constructor() {
+    super();
+    this.state = { topThreePlayers: [] }
+  }
+
+  componentWillMount() {
+    console.log('fetch function goes here to get top three scoring players');
+    this.setState({ topThreePlayers:
+      [
+        { name: 'Charlie', score: 100 },
+        { name: 'Mandy', score: 60 },
+        { name: 'Scott', score: 25 }
+      ]
+    });
+  }
+
+  componentDidMount() {
+    const { topThreePlayers } = this.state;
+    console.log('playa', topThreePlayers);
+  }
+
   render() {
+    const { topThreePlayers } = this.state;
     return (
-      <div>
-        Scoreboard goes here.
-      </div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Player</Table.HeaderCell>
+            <Table.HeaderCell>Score</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {topThreePlayers.map(player => (
+            <Table.Row>
+              <Table.Cell>{player.name}</Table.Cell>
+              <Table.Cell>{player.score}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     );
   }
 };
